293ecea5ba0c7ffb4a304a21750dfef089f696a3
Jon Hill <jon@jonhill.ca>
2018-09-21 11:40:26 +0200

    well we don't need a coordination server anymore

--- pckr_server/db/users.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (
-    ip              CHAR(20),
-    port            int NOT NULL,
-    username        VARCHAR NOT NULL,
-    updated_at      TIMESTAMP NOT NULL,
-    login_token     VARCHAR NOT NULL
-);
